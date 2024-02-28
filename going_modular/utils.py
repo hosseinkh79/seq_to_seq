b35ea3612cdf221dacc23a5f05e5622234e3a31a
Hossein <hossein.kh7935@gmail.com>
2024-02-28 10:42:31 +0330

    change utils

--- going_modular/utils.py
+++ going_modular/utils.py
@@ -9,7 +9,7 @@ def set_seed(seed):
     torch.manual_seed(seed=seed)
     torch.cuda.manual_seed(seed=seed)
 
-
+#HI
 def init_weights(m):
     for name, param in m.named_parameters():
         nn.init.uniform_(param.data, -0.08, 0.08)
